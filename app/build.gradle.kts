410e4e36cd7b532e8c2dda09f133017732757fe4
SFY Labs <dev@sfylabs.example>
2025-08-28 11:50:40 +0000

    build: align to Kotlin 1.9.25 + Compose compiler 1.5.15; add JetBrains compose repo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
     }
 
     buildFeatures { compose = true }
-    composeOptions { kotlinCompilerExtensionVersion = "1.6.11" }
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.15" }
     packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
 
     // Allinea Java/Kotlin a 17 per evitare mismatch JVM target
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 
 plugins {
     id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
 }
 
